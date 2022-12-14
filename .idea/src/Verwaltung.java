9c7194c0a4b62386f74a97f82722932ece08d3d6
dfgraeter <df.graeter@outlook.de>
2022-12-14 09:34:50 +0100

    1.

---
+++ .idea/src/Verwaltung.java
@@ -0,0 +1,22 @@
+public class Verwaltung {
+
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static void main(String[] args) {
+
+        System.out.println("|-------------------------|");
+        System.out.println("|-------Hauptmenü---------|");
+        System.out.println("|                         |");
+        System.out.println("|-(1)bestellen           -|");
+        System.out.println("|-(2)ausliefern          -|");
+        System.out.println("|-(3)informationen       -|");
+        System.out.println("|-(4)loeschen            -|");
+        System.out.println("|-(5)help   (6)ausloggen -|");
+        System.out.println("|-------------------------|");
+
+
+
+
+
+    }
+}
