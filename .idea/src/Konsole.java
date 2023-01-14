0b1e44e238ac16a68f6c310dd3bff06f7e3018ef
YunusEA12 <22alyu1bif@hft-stuttgart.de>
2023-01-14 21:24:05 +0100

    h

--- .idea/src/Konsole.java
+++ .idea/src/Konsole.java
@@ -166,6 +166,7 @@ public class Konsole {
             System.out.println(BLAU+"Was wollen Sie bestellen?"+STANDARD);
             System.out.print("Name: ");
             String name = scanner.nextLine();
+
             System.out.print("Anzahl: ");
             String anzahl = scanner.next();
             verwaltung.bestellenMed(name, anzahl);
